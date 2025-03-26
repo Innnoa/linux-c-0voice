e96fd28d794ea94d15406b26fe38776406fc67b3
innno <banxiaqaq@gmail.com>
2025-03-26 10:58:17 +0800

    1

---
+++ 2/2.1/2.1.1/2.1.1.2/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  return 0;
+}
